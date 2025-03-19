b95fa53f1224556c046ee4f52bc1dbf7dc9ba7d4
AyushiPatel <patelayushi1876@gmail.com>
2025-03-19 12:36:00 +0530

    done

---
+++ 03_tailwindprops/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Card from './components/card'
+
+const App = () => {
+  function App(){
+    let myObj ={
+      username : "Ayushi",
+      age:21
+    }
+    let newArray= [1,2,3,4]
+
+  }
+  return (
+    <>
+    <Card username="hello" />
+    <Card username="AyushiPatel" ></Card>
+    
+   </>
+  )
+}
+
+export default App
---
+++ 03_tailwindprops/src/components/card.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function card({username}) {
+
+
+  return (
+    <>
+   <div className=" flex flex-col items-center rounded-xl  p-4"
+        style={{
+          border: '0.88px solid',
+  
+          backdropFilter: 'saturate(180%) blur(14px)',
+          background: ' #ffffff0d',
+        }}
+      >
+        <div>
+          <img
+            src="https://res.cloudinary.com/ddcg0rzlo/image/upload/v1652470298/9StaF0UBJfih_df0248.gif"
+            alt="nft-gif"
+            width="400"
+            height="400"
+            className="rounded-xl"
+          />
+        </div>
+        <div className="flex flex-col  rounded-b-xl py-4 ">
+          <div className="flex justify-between">
+            <h1 className="font-RubikBold ">{username}</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default card
